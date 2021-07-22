c1d5708cdc322cd212180d635d2ba56e8fdbf781
Saikrishna Panda <itssai91@gmail.com>
2021-07-22 23:28:45 +0530

    Initial commit

---
+++ app/src/main/java/com/infranetstudio/com/countdowntimer/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.infranetstudio.com.countdowntimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import com.infranetstudio.com.countdowntimer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var timer: CountDownTimer? = null
+    private var currentTime: Long = 0
+    private var totalTimer: Long = 10000
+//    private var onTickTime: Long? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnStart.setOnClickListener {
+            countDownTime()
+        }
+        binding.btnPause.setOnClickListener {
+            onPauseTimer()
+        }
+        binding.btnReset.setOnClickListener {
+            onResetTimer()
+        }
+
+    }
+
+    private fun countDownTime() {
+        timer = object : CountDownTimer(totalTimer - currentTime, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                if (binding.btnStart.text == "Resume") {
+                    binding.btnStart.text = "Start"
+                }
+                currentTime = totalTimer - millisUntilFinished
+                binding.tvTimer.text = (millisUntilFinished / 1000).toString()
+                binding.btnStart.isEnabled = false
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Your CountDown Stopped...",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+                binding.btnStart.isEnabled = true
+                timer = null
+                currentTime = 0
+            }
+
+        }.start()
+
+    }
+
+    private fun onPauseTimer() {
+
+        timer?.cancel()
+        if (binding.btnStart.text == "Start") {
+            binding.btnStart.text = "Resume"
+            binding.btnStart.isEnabled = true
+        }
+
+    }
+
+    private fun onResetTimer() {
+        if (timer != null) {
+            timer?.cancel()
+            timer = null
+            currentTime = 0
+            binding.tvTimer.text = (totalTimer / 1000).toString()
+            binding.btnStart.isEnabled = true
+        }
+
+    }
+
+}
